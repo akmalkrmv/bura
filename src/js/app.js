89832556a0798f7e0dbd59aa9c07ad4b3021bfd1
Akmal Karimov <akarimov@competentum.ru>
2018-05-17 13:54:13 +0300

    Some card animation

--- src/js/app.js
+++ src/js/app.js
@@ -6,18 +6,22 @@ var app = new Vue({
                 backside: true,
                 name: 'Bob',
                 cards: [{
+                        backside: true,
                         suit: 'spades',
                         text: 'A'
                     },
                     {
+                        backside: true,
                         suit: 'hearts',
                         text: '10'
                     },
                     {
+                        backside: true,
                         suit: 'clubs',
                         text: 'K'
                     },
                     {
+                        backside: true,
                         suit: 'diamonds',
                         text: '6'
                     }
@@ -28,18 +32,22 @@ var app = new Vue({
                 backside: false,
                 name: 'Jack',
                 cards: [{
+                        backside: false,
                         suit: 'spades',
                         text: '9'
                     },
                     {
+                        backside: false,
                         suit: 'hearts',
                         text: 'A'
                     },
                     {
+                        backside: false,
                         suit: 'clubs',
                         text: 'Q'
                     },
                     {
+                        backside: false,
                         suit: 'diamonds',
                         text: '8'
                     }
